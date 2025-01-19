e4ae0e0066fccae00c393a068aa50189475cae16
gvidas <danieliusgvidas@gmail.com>
2025-01-19 18:28:39 +0000

    fixed player script crashing in map scene, added bookshelf

--- Scripts/buttons.gd
+++ Scripts/buttons.gd
@@ -1,7 +1,7 @@
 extends Button
 
 func _on_PlayButton_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/map.tscn")
 
 func _on_CreditButton_pressed():
 	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
